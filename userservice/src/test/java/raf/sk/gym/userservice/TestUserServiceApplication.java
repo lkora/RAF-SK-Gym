9f71e6dad8fe9e2582def7059144c8d17911f093
djues3 <djues3@gmail.com>
2024-01-08 22:55:36 +0100

    Added user service setup.
    The setup for a new microservice named "User Service" has been initialized. This includes the addition of relevant application test cases and properties, Maven build scripts, and configuration files. The application is designed to use MariaDB for data persistence.

---
+++ userservice/src/test/java/raf/sk/gym/userservice/TestUserServiceApplication.java
@@ -0,0 +1,23 @@
+package raf.sk.gym.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestUserServiceApplication {
+
+	@Bean
+	@ServiceConnection
+	MariaDBContainer<?> mariaDbContainer() {
+		return new MariaDBContainer<>(DockerImageName.parse("mariadb:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(UserServiceApplication::main).with(TestUserServiceApplication.class).run(args);
+	}
+
+}
