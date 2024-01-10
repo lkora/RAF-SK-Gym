da72d8cb49913d48f568e07871a92898e2f9fd5a
djues3 <djues3@gmail.com>
2024-01-10 12:49:34 +0100

    Restore old java source files
    Accidentally deleted java source files when switching to Gradle, restored them.

---
+++ gateway/src/test/java/raf/sk/gym/gateway/TestGatewayApplication.java
@@ -0,0 +1,14 @@
+package raf.sk.gym.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+/**
+ * Provides live reload capabilities
+ *
+ * */
+@TestConfiguration(proxyBeanMethods = false)
+public class TestGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.from(GatewayApplication::main).with(TestGatewayApplication.class).run(args);
+    }
+}
