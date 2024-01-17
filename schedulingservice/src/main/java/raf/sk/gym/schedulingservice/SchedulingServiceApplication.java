75d5fd44474aa78f68b749a9e7da634ac561f2a5
djues3 <djues3@gmail.com>
2024-01-17 03:28:11 +0100

    Scheduling service scaffold.
    Basic stuff needed to start the microservice.

---
+++ schedulingservice/src/main/java/raf/sk/gym/schedulingservice/SchedulingServiceApplication.java
@@ -0,0 +1,13 @@
+package raf.sk.gym.schedulingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingServiceApplication.class, args);
+	}
+
+}
---
+++ schedulingservice/src/test/java/raf/sk/gym/schedulingservice/SchedulingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.sk.gym.schedulingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
